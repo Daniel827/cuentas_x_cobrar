3218df4c1efc308cdd233a5856961a5d926c199b
Daniel Sierra <sierradaniel.02@gmail.com>
2017-06-28 11:18:28 -0500

    Cambio

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -6,14 +6,37 @@ use Illuminate\Http\Request;
 
 class PagoController extends Controller
 {
-  public function __construct() {
-  }
+    public function _construct(){
+      $this -> middleware('auth');
+    }
 
-  public function index(){
-      return view('panel.pagos.index');
-  }
+    public function index(){
+      $pagos=Pago::paginate(10);
+      return view('pagos.index', compact('pagos'));
+    }
 
-  public function create(){
-      return view('panel.pagos.create');
-  }
+    public function create(){
+      return view('pagos.create');
+    }
+
+    public function store(PagoRequest $request){
+      Pago::create($request->all());
+      //return view('productos.index');
+      return Redirect::to('pago');
+    }
+
+    public function edit($id){
+      $pago=Pago::find($id);
+      return view('pagos.edit', compact('pago'));
+    }
+
+    public function update(PagoRequest $request, $id){
+      Pago::updateOrCreate(['idPago'=>$id], $request->all());
+      return Redirect::to('pago');
+    }
+
+    public function destroy($id){
+      Pago::destroy($id);
+      return Redirect::to('pago');
+    }
 }
---
+++ app/Http/Requests/PagoRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PagoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if(Input::has('idPago')){
+            $id=Input::Get('idPago');
+        return [
+            "descripcion"=>"required|min:10|max:300",
+            "fecha"=>"required|date(dd-mm-aaaa)",
+            "numeroPago"=>"required|unique:pagos,numeropago,".$id."idPago",
+            "totalPago"=>"required|numeric"
+            //
+        ];
+     }else{
+        return[
+           "descripcion"=>"required|min:10|max:300",
+            "fecha"=>"required|date(dd-mm-aaaa)",
+            "numeroPago"=>"required|numeric",
+            "totalPago"=>"required|numeric"
+        ];
+     }
+    }
+}
