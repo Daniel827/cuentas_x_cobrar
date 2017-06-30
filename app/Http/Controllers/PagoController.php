af6ae92c161ace47eeb5f8f99024932e5aebb7b7
Daniel Sierra <sierradaniel.02@gmail.com>
2017-06-30 15:26:08 -0500

    Correccion

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -21,65 +21,37 @@ class PagoController extends Controller
       $pagos=Pago::paginate(10);
       return view('panel.pagos.index', compact('pagos'));
     }
-
-<<<<<<< HEAD
-      public function create(){
-      return view('panel.pagos.create');
-=======
-    public function create(){
-      $tipoPagos=TipoPago::get();
-      return view('panel.pagos.create',compact("tipoPagos"));
->>>>>>> 3e4c9cd09a9415c8616f270f89646cd92cd9a96f
-    }
-
-    public function store(PagoRequest $request){
-      Pago::create($request->all());
-<<<<<<< HEAD
-         return Redirect::to('pagos');
-    }
+   public function create(){
+       // llamar al listado de clientes del ws
+       $pagos=Pago::get();
+       return view('panel.pagos.create',compact("pagos"));
+     }
+       
+    
+  
+      public function store(PagoRequest $request){
+        Pago::create($request->all());
+       //return view('productos.index');
+        return Redirect::to('pagos');
+      }
+          
+    
+ 
+    
+      public function edit(){
+       $pagos=Pago::paginate(10);
+       return view('panel.pagos.show', compact('pagos'));
+     }
+ 
+     public function update(PagoRequest $request, $id){
+       Pago::updateOrCreate(['idPago'=>$id], $request->all());
+       return Redirect::to('pagos');
+     }
+ 
+     public function destroy($id){
+       Pago::destroy($id);
+       return Redirect::to('pago');
+     }
 
    
-     public function edit(){
-      $pagos=Pago::paginate(10);
-      return view('panel.pagos.show', compact('pagos'));
-    }
-
-    public function update(PagoRequest $request, $id){
-      Pago::updateOrCreate(['idPago'=>$id], $request->all());
-      return Redirect::to('pagos');
-    }
-
-    public function destroy($id){
-      Pago::destroy($id);
-      return Redirect::to('pago');
-    }
 }
-=======
-      $idPago=Pago::max("idPago");
-      $total=0;
-      for($i=0;$i<sizeof($request->idFactura);$i++ ){
-        $idFactura=$request->idFactura[$i];
-        $idTipoPago=$request->idTipoPago[$i];
-        $pago=$request->pago[$i];
-        DetallePago::create(["idFactura"=>$idFactura,"idTipoPago"=>$idTipoPago,"pago"=>$pago]);
-        $total+=$pago;
-      }
-      Pago::createOrUpdate(["idPago"=>$idPago],["pago"=>$total]);
-      return Redirect::to('pago');
-    }
-
-    public function edit($id){
-      $pago=Pago::find($id);
-      return view('pagos.edit', compact('pago'));
-    }
-
-    public function show($id){
-      return view('panel.pagos.show',compact(Pago::findOrFail($id)));
-    }
-
-    public function update(PagoRequest $request, $id){
-      Pago::updateOrCreate(['idPago'=>$id], $request->all());
-      return Redirect::to('pago');
-    }
-}
->>>>>>> 3e4c9cd09a9415c8616f270f89646cd92cd9a96f
