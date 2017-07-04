f676f118322b0c37b46bb9197b64eaf73579d33b
cristofima <cristofima@hotmail.com>
2017-07-04 00:13:25 -0500

    Reglas de validación de cédula/ruc y clave actual

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot(){
+      $this->validateCedula();
+      $this->validateCurrent_Password();
     }
 
     /**
@@ -24,5 +24,45 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    private function validateCurrent_Password(){
+        Validator::extend('current_password',function($attribute,$value,$parametes){
+          return Hash::check($value, Auth::user()->password);
+        });
+    }
+
+    private function validateCedula(){
+      Validator::extend('cedula',function($attribute,$value,$parametes){
+          if(preg_match('/[0-2][0-9]{9}(001)?/',$value)){
+            $c1=0;
+            $c2=0;
+            $f=0;
+            $cedula=$value;
+            for($i=0;$i<9;$i++){
+                $a=$cedula[$i];
+                  if($i%2==0){
+                      $d=$a*2;
+                      if($d<9){
+                          $c1+=$d;
+                      }else{
+                          $c1+=$d-9;
+                      }
+                  }else{
+                      $c2+=$a;
+                  }
+              }
+              $e=$c1+$c2;
+              for($j=10;$j<=60;$j=$j+10){
+                  if($e<=$j){
+                      $f=$j-$e;
+                      break;
+                  }
+              }
+              return $cedula[9]==$f;
+          }else{
+            return false;
+          }
+      });
     }
 }
