8b04b6d7bc5e0a3281a58a42d17f499ece836a2e
cristofima <cristofima@hotmail.com>
2017-07-31 06:11:09 -0500

    Diseño reporte movimientos

--- resources/views/panel/reportes.blade.php
+++ resources/views/panel/reportes.blade.php
@@ -43,7 +43,7 @@
                         </div>
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">Generar</button>
+                                <a type="submit" target="_blank" class="btn btn-primary">Generar</button>
                             </div>
                         </div>
                     </form>
