82d217d115dbe6eeaa7b2f7ac7082058ee5adc5b
cury <curytravez_t.v@hotmail.com>
2017-08-03 09:32:31 -0500

    Responsive Pagos

--- resources/views/panel/pagos/index.blade.php
+++ resources/views/panel/pagos/index.blade.php
@@ -16,9 +16,10 @@
     </div>
     <div class="clearfix"></div>
     <div class="row">
-        <div class="card-box table-responsive">
+       
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
+                 <div class="card-box table-responsive">
                 <div class="x_title">
                     <h2>Listado de Pagos</h2>
                     <ul class="nav navbar-right panel_toolbox">
